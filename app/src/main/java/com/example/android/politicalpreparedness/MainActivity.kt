16bf0dfcf6e111bf5caa764d117e74586d9d8c0b
EyvindEmilio <eyvind.coaquira@gmail.com>
2021-09-22 19:01:53 -0400

    Added animation for launch fragment

--- app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.android.politicalpreparedness
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.android.politicalpreparedness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
